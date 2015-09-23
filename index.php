03c4afb46275987c3385a266a4704be669b61bc8
snakeplayer <chauche.benoit@gmail.com>
2015-09-23 16:06:14 +0200

    Functions.php : finalisation de la fonction AssocToHtml
    index.php : swagg
    modifier_users.php : création de la page
    style.css : optimisation du style
    users.php : création de la page

--- index.php
+++ index.php
@@ -6,19 +6,13 @@ require_once 'Functions.php';
 $message = null;
 
 if (isset($_POST['valider'])) {
-    if ($_POST['nom'] != null && $_POST['prenom'] != null && $_POST['dateNaissance'] != null && $_POST['description'] != null && $_POST['email'] != null && $_POST['pseudo'] != null && $_POST['pwd'] != null)
-    {
+    if ($_POST['nom'] != null && $_POST['prenom'] != null && $_POST['dateNaissance'] != null && $_POST['description'] != null && $_POST['email'] != null && $_POST['pseudo'] != null && $_POST['pwd'] != null) {
         InsertUser($_POST['nom'], $_POST['prenom'], $_POST['dateNaissance'], $_POST['description'], $_POST['email'], $_POST['pseudo'], $_POST['pwd']);
         $message = "Formulaire envoyé !";
-    }
- else {
+    } else {
         $message = 'Veuillez renseigner tous les champs !';
     }
 }
-
-$tabUser = GetUsers();
-echo AssocToHtml($tabUser);
-
 ?>
 
 <!DOCTYPE html>
@@ -31,74 +25,76 @@ echo AssocToHtml($tabUser);
     </head>
     <body>
         <div id="corpsForm">
-            <form id="formFormualire" method="post" action="#">
-                <table id="tableFormulaire">
-                    <th>
-                        Formulaire d'inscription
-                    </th>
-                    <tr>
-                        <td>_______________________</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="nom">Nom :</label>
-                        </td>
-                        <td>
-                            <input type="text" name="nom" id="nom" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="prenom">Prénom : </label>
-                        </td>
-                        <td>
-                            <input type="text" name="prenom" id="prenom" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="dateNaissance">Date de naissance : </label>
-                        </td>
-                        <td>
-                            <input type="date" name="dateNaissance" id="dateNaissance" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="description">Petite description : </label>
-                        </td>
-                        <td>
-                            <textarea name="description" id="description"></textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email"> Email : </label>
-                        </td>
-                        <td>
-                            <input type="email" name="email" id="email" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="pseudo">Pseudo :</label>
-                        </td>
-                        <td>
-                            <input type="text" name="pseudo" id="pseudo" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="pwd"> Mot de passe : </label>
-                        </td>
-                        <td>
-                            <input type="password" name="pwd" id="pwd" />
-                        </td>
-                    </tr>
-                </table>
-                <input type="submit" name='valider' value='Valider' id="btnValider"/><input type="button" name='annuler' value='Annuler' id="btnAnnuler"/>
-                <?php echo $message ?>
-            </form>
+            <div id="insideCorpsForm">
+                <form id="formFormualire" method="post" action="#">
+                    <table id="tableFormulaire">
+                        <th>
+                            Formulaire d'inscription
+                        </th>
+                        <tr>
+                            <td>_______________________</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="nom">Nom :</label>
+                            </td>
+                            <td>
+                                <input type="text" name="nom" id="nom" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="prenom">Prénom : </label>
+                            </td>
+                            <td>
+                                <input type="text" name="prenom" id="prenom" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="dateNaissance">Date de naissance : </label>
+                            </td>
+                            <td>
+                                <input type="date" name="dateNaissance" id="dateNaissance" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="description">Petite description : </label>
+                            </td>
+                            <td>
+                                <textarea name="description" id="description"></textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="email"> Email : </label>
+                            </td>
+                            <td>
+                                <input type="email" name="email" id="email" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="pseudo">Pseudo :</label>
+                            </td>
+                            <td>
+                                <input type="text" name="pseudo" id="pseudo" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="pwd"> Mot de passe : </label>
+                            </td>
+                            <td>
+                                <input type="password" name="pwd" id="pwd" />
+                            </td>
+                        </tr>
+                    </table>
+                    <input type="submit" name='valider' value='Valider' id="btnValider"/><input type="button" name='annuler' value='Annuler' id="btnAnnuler"/>
+<?php echo $message ?>
+                </form>
+            </div>
         </div>
     </body>
 </html>
